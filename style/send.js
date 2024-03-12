422696ddf98c7553fb11d8beefb93b3c440db919
AmilyCC <89175058+AmilyCC@users.noreply.github.com>
2024-03-12 11:41:02 +0800

    Update send.js

--- style/send.js
+++ style/send.js
@@ -19,16 +19,30 @@ function sendtype(){
     let type = document.getElementById('type').value
     return type 
 }
-function sendInput(){
+function InputExist(){
+    const images = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp'];
     let input = document.getElementById('userInput').value
+    const lowerCaseUrl = input.toLowerCase();
     if(input == ''){
         alert('請輸入文字')
-    }else{
-        if (sendtype()== "text"){
-            return `text=${input}`
-        }else{
-            return `downloadUrl=${input}&previewUrl=${input}`
+    }else if(sendtype() == "image"){
+        for (let image of images) {
+            if (lowerCaseUrl.endsWith(image)) {
+                return ;
+            }else{
+                alert('請輸入正確的圖片網址')
+            }
         }
+    }else{
+        return
     }
-    
+}
+
+function sendInput(){
+    InputExist()
+    if (sendtype() == "text"){
+        return `text=${input}`
+    }else{
+        return `downloadUrl=${input}&previewUrl=${input}`
+    }
 }
